15627bd3d9c0b27805b1afa80a72c9d3043805b9
Matheus Menezes <136189680+mttwmenezes@users.noreply.github.com>
2025-07-11 13:21:19 -0300

    feat(home): implement top-level navigation
    Implement infrastructure to allow the user to navigate between the top-level destinations using the Home navigation bar.

--- app/src/main/java/com/github/mttwmenezes/apodbrowser/feature/home/view/HomeActivity.kt
+++ app/src/main/java/com/github/mttwmenezes/apodbrowser/feature/home/view/HomeActivity.kt
@@ -3,6 +3,10 @@ package com.github.mttwmenezes.apodbrowser.feature.home.view
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.github.mttwmenezes.apodbrowser.R
 import com.github.mttwmenezes.apodbrowser.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,10 +15,19 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        configureNavController()
+        binding.navigationBar.setupWithNavController(navController)
+    }
+
+    private fun configureNavController() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHost.navController
     }
 }
