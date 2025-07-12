7cf827d9e43445438f1b81372befbf1f82fa4c50
Matheus Menezes <136189680+mttwmenezes@users.noreply.github.com>
2025-07-12 13:31:48 -0300

    feat: create Apod data model
    Create a data structure for representing Apods at the application level.

---
+++ app/src/main/java/com/github/mttwmenezes/apodbrowser/data/model/Apod.kt
@@ -0,0 +1,14 @@
+package com.github.mttwmenezes.apodbrowser.data.model
+
+import java.io.Serializable
+
+data class Apod(
+    val title: String,
+    val date: String,
+    val url: String,
+    val hdUrl: String?,
+    val mediaType: String,
+    val explanation: String,
+    val thumbnailUrl: String?,
+    val copyright: String?
+) : Serializable
