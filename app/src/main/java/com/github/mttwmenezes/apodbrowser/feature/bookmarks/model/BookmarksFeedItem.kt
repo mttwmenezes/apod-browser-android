7e68ac9bc37263c547af49809ba5b3ac60c2aaae
Matheus Menezes <136189680+mttwmenezes@users.noreply.github.com>
2025-07-21 15:36:19 -0300

    feat(bookmarks): adjust feed item
    - Add the Apod's media type to the feed item data model.
    - Adjust the feed item layout to add the Apod's media type.
    - Implement behavior to change the feed item image height when the image has loaded.
    
    These changes are necessary for creating a better experience while browsing bookmarks in a staggered grid.

--- app/src/main/java/com/github/mttwmenezes/apodbrowser/feature/bookmarks/model/BookmarksFeedItem.kt
+++ app/src/main/java/com/github/mttwmenezes/apodbrowser/feature/bookmarks/model/BookmarksFeedItem.kt
@@ -5,6 +5,7 @@ sealed class BookmarksFeedItem(val id: String) {
     data class Apod(
         val itemId: String,
         val imageUrl: String?,
-        val title: String
+        val title: String,
+        val mediaType: String
     ) : BookmarksFeedItem(id = itemId)
 }
--- app/src/main/java/com/github/mttwmenezes/apodbrowser/feature/bookmarks/view/feed/BookmarksFeedViewHolder.kt
+++ app/src/main/java/com/github/mttwmenezes/apodbrowser/feature/bookmarks/view/feed/BookmarksFeedViewHolder.kt
@@ -1,6 +1,8 @@
 package com.github.mttwmenezes.apodbrowser.feature.bookmarks.view.feed
 
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.updateLayoutParams
 import androidx.recyclerview.widget.RecyclerView
 import coil3.load
 import com.github.mttwmenezes.apodbrowser.databinding.FeedItemBookmarkBinding
@@ -16,9 +18,28 @@ sealed class BookmarksFeedViewHolder(itemView: View) : RecyclerView.ViewHolder(i
             item: BookmarksFeedItem.Apod,
             onItemClickListener: (String) -> Unit
         ) = with(binding) {
-            image.load(item.imageUrl)
+            image.load(item.imageUrl) {
+                listener(
+                    onStart = { onImageLoadStarted() },
+                    onSuccess = { _, _ -> onImageLoadSuccess() }
+                )
+            }
             titleLabel.text = item.title
             root.setOnClickListener { onItemClickListener(item.itemId) }
+        }
+
+        private fun onImageLoadStarted() = with(binding.image) {
+            updateLayoutParams<ConstraintLayout.LayoutParams> {
+                height = ConstraintLayout.LayoutParams.MATCH_CONSTRAINT
+                dimensionRatio = "16:9"
+            }
+        }
+
+        private fun onImageLoadSuccess() = with(binding.image) {
+            updateLayoutParams<ConstraintLayout.LayoutParams> {
+                height = ConstraintLayout.LayoutParams.WRAP_CONTENT
+                dimensionRatio = null
+            }
         }
     }
 }
