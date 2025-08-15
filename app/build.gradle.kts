a281471d035298241669c6b7af27c4165bdabaa0
Matheus Menezes <136189680+mttwmenezes@users.noreply.github.com>
2025-08-15 15:10:57 -0300

    build: set initial stable version
    Set the project's initial version name to "stable-1.0.0".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "stable-1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
